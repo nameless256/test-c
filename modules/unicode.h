d2e3deafafc9d6f3715cd24419644d266de0d532
nameless256 <2245616155@qq.com>
2024-11-16 17:43:45 +0800

    [unicode] 添加注释

--- modules/unicode.h
+++ modules/unicode.h
@@ -22,19 +22,26 @@ uint32_t unicodeGetCodePointByUtf8(const uint8_t **utf8);
 /**
  * @param codePoint unicode 码点
  * @param[out] utf8 传入写指针的地址 方便连续操作 指针会自动后移
- * @param utf8Size utf8 剩余缓存大小
+ * @param utf8Size 剩余缓冲区码元个数 (sizeof(buffer) / sizeof(uint8_t))
  * @return 是否成功
  * @retval true 失败
  */
 bool unicodeSetUtf8ByCodePoint(uint32_t codePoint, uint8_t **utf8, size_t utf8Size);
 
 /**
- * @see unicodeGetCodePointByUtf8
+ * @param[in] utf16 传入读指针的地址 方便连续操作 指针会自动后移
+ * @return unicode 码点
+ * @retval UNICODE_ERROR 错误
+ * @note 虽然错误码是 UNICODE_ERROR 但建议还是通过是否大于 UNICODE_MAX 判断是否为错误
  */
 uint32_t unicodeGetCodePointByUtf16(const uint16_t **utf16);
 
 /**
- * @see unicodeSetUtf8ByCodePoint
+ * @param codePoint unicode 码点
+ * @param[out] utf16 传入写指针的地址 方便连续操作 指针会自动后移
+ * @param utf16Size 剩余缓冲区码元个数 (sizeof(buffer) / sizeof(uint16_t))
+ * @return 是否成功
+ * @retval true 失败
  */
 bool unicodeSetUtf16ByCodePoint(uint32_t codePoint, uint16_t **utf16, size_t utf16Size);
 
