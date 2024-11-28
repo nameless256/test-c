37c27bcc498212b04c721efa43b5dcf6ba329d4d
xiaoxianghui <2245616155@qq.com>
2024-11-28 10:11:27 +0800

    [oop] 细节优化

--- main.c
+++ main.c
@@ -15,7 +15,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include "unicode.h"
+#include "oop_usage.h"
 
 /**
  * @defgroup Main
@@ -26,7 +26,7 @@ int main() {
     system("chcp 65001");
     clock_t start = clock();
 
-    unicodeUsage();
+    oopUsage();
 
     clock_t stop = clock();
     double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
