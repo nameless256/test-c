6690d4c62563ccd8ac10b523717e33c218ec93d5
xiaoxianghui <2245616155@qq.com>
2024-12-10 16:42:51 +0800

    [pthread] 测试

--- main.c
+++ main.c
@@ -15,18 +15,44 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include "unicode.h"
+#include <pthread.h>
 
 /**
  * @defgroup Main
  * @{
  */
 
+// 线程函数
+void* thread_function(void* arg) {
+    int thread_id = *(int*)arg;
+    printf("Thread %d is running\n", thread_id);
+    pthread_exit(NULL);
+}
+
 int main() {
     system("chcp 65001");
     clock_t start = clock();
 
-    unicodeUsage();
+    pthread_t threads[5];
+    int thread_ids[5];
+    int result;
+
+    // 创建5个线程
+    for (int i = 0; i < 5; i++) {
+        thread_ids[i] = i;
+        result = pthread_create(&threads[i], NULL, thread_function, (void*)&thread_ids[i]);
+        if (result != 0) {
+            printf("Error creating thread %d\n", i);
+            exit(-1);
+        }
+    }
+
+    // 等待所有线程完成
+    for (int i = 0; i < 5; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    printf("All threads have finished execution\n");
 
     clock_t stop = clock();
     double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
