a5984f27c7c4196a0052a327a63282a33ae3a382
xiaoxianghui <2245616155@qq.com>
2025-01-10 16:27:07 +0800

    [game] word search 优化

--- game/word_search.c
+++ game/word_search.c
@@ -46,12 +46,16 @@ typedef struct wordDir {
     bool positive;
 } wordDir;
 
-typedef struct wordInfo {
-    const char *word;
-    bool found;
+typedef struct wordSelectArea {
     wordDir dir;
     uint8_t length;
-    int pOfs, p0;
+    int ofs, x;
+} wordSelectArea;
+
+typedef struct wordInfo {
+    const char *word;
+    wordSelectArea area;
+    bool found;
 } wordInfo;
 
 #define WORD_MAX 4
@@ -64,10 +68,8 @@ struct wordSearch {
     int row, col;
     wordInfo wordTab[WORD_MAX];
     uint8_t wordCount;
+    wordSelectArea area;
     bool touch;
-    wordDir dir;
-    uint8_t length;
-    int pOfs, p0;
 };
 
 static const char *getRandWord(uint8_t i) {
@@ -86,18 +88,18 @@ static const char *getRandWord(uint8_t i) {
 
 static bool placeWord(wordSearch *obj, uint8_t i) {
     for (int j = 0; j < i; ++j) {
-        if (obj->wordTab[j].length == obj->wordTab[i].length) {
+        if (obj->wordTab[j].area.length == obj->wordTab[i].area.length) {
             if (0 == strcmp(obj->wordTab[j].word, obj->wordTab[i].word)) return true;
         }
     }
-    for (int j = 0; j < obj->wordTab[i].length; ++j) {
-        int x = obj->wordTab[i].pOfs, y = obj->wordTab[i].p0 + (obj->wordTab[i].dir.positive ? j : -j);
-        if (obj->wordTab[i].dir.horizontal) SWAP(x, y);
+    for (int j = 0; j < obj->wordTab[i].area.length; ++j) {
+        int x = obj->wordTab[i].area.ofs, y = obj->wordTab[i].area.x + (obj->wordTab[i].area.dir.positive ? j : -j);
+        if (obj->wordTab[i].area.dir.horizontal) SWAP(x, y);
         if (obj->alphabet[y][x] != 0 && obj->alphabet[y][x] != obj->wordTab[i].word[j]) return true;
     }
-    for (int j = 0; j < obj->wordTab[i].length; ++j) {
-        int x = obj->wordTab[i].pOfs, y = obj->wordTab[i].p0 + (obj->wordTab[i].dir.positive ? j : -j);
-        if (obj->wordTab[i].dir.horizontal) SWAP(x, y);
+    for (int j = 0; j < obj->wordTab[i].area.length; ++j) {
+        int x = obj->wordTab[i].area.ofs, y = obj->wordTab[i].area.x + (obj->wordTab[i].area.dir.positive ? j : -j);
+        if (obj->wordTab[i].area.dir.horizontal) SWAP(x, y);
         obj->alphabet[y][x] = obj->wordTab[i].word[j];
     }
     return false;
@@ -124,15 +126,16 @@ void wordSearchSetLevel(wordSearch *obj, uint8_t level) {
     obj->wordCount = 4;
     for (int i = 0; i < obj->wordCount; ++i) {
         uint8_t randWordIdx = wordBaseIdx + (i > (WORD_MAX - 1 - (obj->level - 1) % WORD_MAX) ? 1 : 0);
+        wordInfo *info = &obj->wordTab[i];
         do {
-            obj->wordTab[i].word = getRandWord(randWordIdx);
-            obj->wordTab[i].found = false;
-            obj->wordTab[i].dir.horizontal = rand() % 2;
-            obj->wordTab[i].dir.positive = rand() % 2;
-            obj->wordTab[i].length = strlen(obj->wordTab[i].word);
-            obj->wordTab[i].pOfs = rand() % (obj->wordTab[i].dir.horizontal ? obj->col : obj->row);
-            uint8_t p0max = ((obj->wordTab[i].dir.horizontal ? obj->row : obj->col) - obj->wordTab[i].length);
-            obj->wordTab[i].p0 = rand() % p0max + (obj->wordTab[i].dir.positive ? 0 : (obj->wordTab[i].length - 1));
+            info->word = getRandWord(randWordIdx);
+            info->found = false;
+            info->area.dir.horizontal = rand() % 2;
+            info->area.dir.positive = rand() % 2;
+            info->area.length = strlen(info->word);
+            info->area.ofs = rand() % (info->area.dir.horizontal ? obj->col : obj->row);
+            uint8_t lengthMax = ((info->area.dir.horizontal ? obj->row : obj->col) - info->area.length);
+            obj->wordTab[i].area.x = rand() % lengthMax + (info->area.dir.positive ? 0 : (info->area.length - 1));
         } while (placeWord(obj, i));
     }
 }
@@ -164,10 +167,10 @@ void wordSearchSetLineStart(wordSearch *obj, int x, int y) {
     if (x < 0) x = 0;
     if (y >= obj->row) y = obj->row - 1;
     if (y < 0) y = 0;
-    obj->dir.horizontal = true;
-    obj->dir.positive = true;
-    obj->length = 1;
-    obj->pOfs = y, obj->p0 = x;
+    obj->area.dir.horizontal = true;
+    obj->area.dir.positive = true;
+    obj->area.length = 1;
+    obj->area.ofs = y, obj->area.x = x;
     obj->touch = true;
 }
 
@@ -178,20 +181,20 @@ bool wordSearchSetLineEnd(wordSearch *obj, int x, int y) {
     if (y >= obj->row) y = obj->row - 1;
     if (y < 0) y = 0;
     int pOfs = y, p0 = x;
-    if (!obj->dir.horizontal) SWAP(pOfs, p0);
+    if (!obj->area.dir.horizontal) SWAP(pOfs, p0);
     bool needUpdate = false;
-    if (ABS(pOfs - obj->pOfs) > ABS(p0 - obj->p0)) {
-        obj->dir.horizontal = !obj->dir.horizontal;
-        SWAP(obj->pOfs, obj->p0);
+    if (ABS(pOfs - obj->area.ofs) > ABS(p0 - obj->area.x)) {
+        obj->area.dir.horizontal = !obj->area.dir.horizontal;
+        SWAP(obj->area.ofs, obj->area.x);
         SWAP(pOfs, p0);
         needUpdate = true;
     }
-    if (obj->dir.positive != p0 > obj->p0) {
-        obj->dir.positive = p0 > obj->p0;
+    if (obj->area.dir.positive != p0 > obj->area.x) {
+        obj->area.dir.positive = p0 > obj->area.x;
         needUpdate = true;
     }
-    if (obj->length != 1 + ABS(p0 - obj->p0)) {
-        obj->length = 1 + ABS(p0 - obj->p0);
+    if (obj->area.length != 1 + ABS(p0 - obj->area.x)) {
+        obj->area.length = 1 + ABS(p0 - obj->area.x);
         needUpdate = true;
     }
     return needUpdate;
@@ -201,11 +204,11 @@ bool wordSearchFoundWord(wordSearch *obj) {
     if (!obj->touch) return false;
     for (int i = 0; i < obj->wordCount; ++i) {
         if (obj->wordTab[i].found) continue;
-        if (obj->wordTab[i].dir.horizontal != obj->dir.horizontal) continue;
-        if (obj->wordTab[i].dir.positive != obj->dir.positive) continue;
-        if (obj->wordTab[i].length != obj->length) continue;
-        if (obj->wordTab[i].pOfs != obj->pOfs) continue;
-        if (obj->wordTab[i].p0 != obj->p0) continue;
+        if (obj->wordTab[i].area.dir.horizontal != obj->area.dir.horizontal) continue;
+        if (obj->wordTab[i].area.dir.positive != obj->area.dir.positive) continue;
+        if (obj->wordTab[i].area.length != obj->area.length) continue;
+        if (obj->wordTab[i].area.ofs != obj->area.ofs) continue;
+        if (obj->wordTab[i].area.x != obj->area.x) continue;
         obj->wordTab[i].found = true;
         break;
     }
