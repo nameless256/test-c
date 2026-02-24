fe4b863e67003198717f6adbd74dbb06667471ee
xiaoxianghui <2245616155@qq.com>
2026-02-24 18:06:59 +0800

    拓展

--- common/mcr_util.h
+++ common/mcr_util.h
@@ -186,8 +186,11 @@
 
 #define asIs(...) __VA_ARGS__
 
-#define __mcrVaCount(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, N, ...) N
-#define _mcrVaCount(...) __mcrVaCount(__VA_ARGS__, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
+#define __mcrVaCount(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, _15, _16, \
+    _17, _18, _19, _20, _21, _22, _23, _24, _25, _26, _27, _28, _29, _30, _31, _32, N, ...) N
+#define _mcrVaCount(...) __mcrVaCount(__VA_ARGS__, \
+    32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, \
+    16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1)
 #define mcrVaCount(...) pred(_mcrVaCount(dummy, ##__VA_ARGS__))
 
 #define _mcrVaTupleUnpack(...) __VA_ARGS__
