e0e7f84d0884a2be654b2ad29d22d785cd6a7259
xiaoxianghui <2245616155@qq.com>
2026-02-26 11:09:57 +0800

    单头文件宏控进行访问控制不合适

---
+++ common/structure/vector.c
@@ -0,0 +1,7 @@
+//
+// Created by CodingDev on 2026/2/26.
+//
+
+#include "vector.h"
+
+/// \todo
---
+++ common/structure/vector.h
@@ -0,0 +1,8 @@
+//
+// Created by CodingDev on 2026/2/26.
+//
+
+#ifndef TEST_C_VECTOR_H
+#define TEST_C_VECTOR_H
+
+#endif //TEST_C_VECTOR_H
