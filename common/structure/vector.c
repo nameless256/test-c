d5a99243b5a0f86ab5b0331370ce4ac321f3f047
xiaoxianghui <2245616155@qq.com>
2026-03-02 10:47:43 +0800

    vector

--- common/structure/vector.c
+++ common/structure/vector.c
@@ -9,9 +9,22 @@
 
 #include "vector.h"
 
+void vector_dtor(void *v) {
+    if (v) free(vector_getMeta(v));
+}
+
 void vector_cleanup(void *p2p) {
     void *self = *((void **) p2p);
-    if (self) free(vector_getMeta(self));
+    vector_dtor(self);
+}
+
+void vector_reserve(void *v, size_t c) {
+    if (c == vector_capacity(v)) return;
+    size_t ex = vector_capacity(v) + (vector_capacity(v) >> 1);
+    if (ex < c) ex = c;
+    /// \todo alloc_safe return true 时异常抛出
+    if (alloc_safe((void **) &v, vector_getMeta(v)->typeSize * ex, sizeof(vectorMeta))) return;
+    vector_capacity(v) = ex;
 }
 
 void vectorBase_ctor(vectorBase *self, size_t typeSize, size_t capacity, void *value) {
--- common/structure/vector.h
+++ common/structure/vector.h
@@ -15,20 +15,26 @@ typedef struct vectorMeta {
 
 /// \todo alloc_safe return true 时异常抛出
 /// \todo 复杂对象析构及拷贝
-#define _vector2(t, n, c, v)                                                     \
-t *n __attribute__((cleanup(vector_cleanup))) = NULL;                            \
+#define _vector_ctor2(n, c, v)                                                   \
 do {                                                                             \
-    if (alloc_safe((void **) &n, sizeof(t) * c, sizeof(vectorMeta))) break;      \
+    (n) = NULL;                                                                  \
+    if (alloc_safe((void **) &(n), sizeof(*(n)) * c, sizeof(vectorMeta))) break; \
     vector_capacity(n) = c;                                                      \
     vector_size(n) = c;                                                          \
-    vector_getMeta(n)->typeSize;                                                 \
+    vector_getMeta(n)->typeSize = sizeof(*(n));                                  \
     if (c == 0) break;                                                           \
-    n[0] = v;                                                                    \
-    fillMemoryWithPattern(n, sizeof(t), c, &n[0]);                               \
+    (n)[0] = v;                                                                  \
+    fillMemoryWithPattern(n, sizeof(*(n)), c, &(n)[0]);                          \
 } while (0)
-#define _vector1(t, n, c) _vector2(t, n, c, 0)
-#define _vector0(t, n) _vector1(t, n, 0)
-#define vector(t, n, ...) cat2(_vector, mcrVaCount(__VA_ARGS__)) (t, n, ##__VA_ARGS__)
+#define _vector_ctor1(n, c) _vector_ctor2(n, c, 0)
+#define _vector_ctor0(n) _vector_ctor1(n, 0)
+#define vector_ctor(n, ...) cat2(_vector_ctor, mcrVaCount(__VA_ARGS__)) (n, ##__VA_ARGS__)
+
+#define vector(t, n, ...)                      \
+t *n __attribute__((cleanup(vector_cleanup))); \
+vector_ctor(n, ##__VA_ARGS__);
+
+void vector_dtor(void *v);
 
 void vector_cleanup(void *p2p);
 
@@ -37,16 +43,9 @@ void vector_cleanup(void *p2p);
 #define vector_empty(v) (vector_size(v) == 0)
 
 /// \todo NULL 时异常抛出
-#define vector_at(v, i) *(i < vector_size(v) ? &v[i] : NULL)
+#define vector_at(v, i) *(i < vector_size(v) ? &(v)[i] : NULL)
 #define vector_front(v) vector_at(v, 0)
 #define vector_back(v) vector_at(v, vector_size(v) - 1)
-#define vector_data(v) v
+#define vector_data(v) (v)
 
-void vector_reserve(void *v, size_t c) {
-    if (c == vector_capacity(v)) return;
-    size_t ex = vector_capacity(v) + (vector_capacity(v) >> 1);
-    if (ex < c) ex = c;
-    /// \todo alloc_safe return true 时异常抛出
-    if (alloc_safe((void **) &v, vector_getMeta(v)->typeSize * ex, sizeof(vectorMeta))) return;
-    vector_capacity(v) = ex;
-}
+void vector_reserve(void *v, size_t c);
