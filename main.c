0a401ccca534c76b2515497f52b9bd0fde948a74
xiaoxianghui <2245616155@qq.com>
2026-07-29 15:00:23 +0800

    chore: 代码整理

--- main.c
+++ main.c
@@ -16,106 +16,12 @@
 #include <stdio.h>
 #include <time.h>
 
-#include "vector.h"
+#include "meta.h"
 
 /**
  * @defgroup Main
  * @{
  */
-
-void test() {
-    vector(double, map, 10, 3.14);
-    for (int i = 0; i < 10; ++i) {
-        printf("%.2f ", vector_at(map, i));
-    }
-    printf("\n");
-    for (int i = 0; i < 10; ++i) {
-        vector_at(map, i) = 1.25;
-    }
-    for (int i = 0; i < 10; ++i) {
-        printf("%.2f ", vector_at(map, i));
-    }
-    printf("\n");
-}
-
-typedef struct string string;
-
-struct string {
-    objBase base;
-    size_t size;
-    size_t length;
-    uint8_t *data;
-};
-
-bool string_ctor(objBase *obj) {
-    return false;
-}
-
-void string_dtor(objBase *obj) {
-    string *self = (string*)obj;
-    if (self->data) {
-        free(self->data);
-        self->data = NULL;
-    }
-    self->length = 0;
-    self->size = 0;
-}
-
-bool string_copy(objBase *obj, objBase *other) {
-    string *self = (string*)obj;
-    string *src = (string*)other;
-    if (alloc_safe((void**)&self->data, src->size, 0)) return true;
-    memcpy(self->data, src->data, src->size);
-    self->length = src->length;
-    self->size = src->size;
-    return false;
-}
-
-static const meta_field string_fields[] = {
-    {
-        .base = {
-            .dsc = "size_t size",
-            .name = "size"
-        },
-        .ofs = structOfsOf(string, size)
-    },
-    {
-        .base = {
-            .dsc = "size_t length",
-            .name = "length"
-        },
-        .ofs = structOfsOf(string, length)
-    },
-    {
-        .base = {
-            .dsc = "uint8_t *data",
-            .name = "data"
-        },
-        .ofs = structOfsOf(string, data)
-    },
-};
-
-meta_class string_meta = {
-    .base = {
-        .name = "string",
-        .size = sizeof(string),
-        .quals = qual_Null,
-        .id = typeId_Class,
-    },
-    .ctor = string_ctor,
-    .dtor = string_dtor,
-    .copy = string_copy,
-    .cnt = 3,
-    .fields = string_fields,
-};
-
-#define structName test1
-#define structMember(f) \
-mcrDispatch(f, a, int8_t a) \
-mcrDispatch(f, b, char* b)
-#define structMataRemain
-#include "def_struct.h"
-#include "def_meta_struct.h"
 
 int main() {
     system("chcp 65001");
