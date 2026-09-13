316dfd479237c169cd66fadc1e588296f085b32b
Doug <2245616155@qq.com>
2026-09-13 10:22:57 +0800

    refactor(reflection): 重构元数据反射系统中的虚函数表定义
    - 将 objBase_vtab 结构体定义从 meta.h 移至 obj.h 文件
    - 删除了不再使用的访问调度宏定义和相关函数声明
    - 简化了 objBase 结构体中虚函数表指针的类型定义
    - 添加了对象方法调用宏 $(self, func, ...) 的定义
    - 重新组织了头文件包含关系以优化代码结构

--- common/framework/reflection/meta.h
+++ common/framework/reflection/meta.h
@@ -102,51 +102,8 @@ struct meta_struct {
 
 typedef struct objBase objBase;
 
-#define _accessDispatch00(f0, f1, ...) f1(__VA_ARGS__)
-#define _accessDispatch01(f0, f1, ...) f1(__VA_ARGS__)
-#define _accessDispatch02(f0, f1, ...) f1(__VA_ARGS__)
-#define _accessDispatch10(f0, f1, ...) f0(__VA_ARGS__)
-#define _accessDispatch11(f0, f1, ...) f1(__VA_ARGS__)
-#define _accessDispatch12(f0, f1, ...) f1(__VA_ARGS__)
-#define _accessDispatch20(f0, f1, ...) f0(__VA_ARGS__)
-#define _accessDispatch21(f0, f1, ...) f0(__VA_ARGS__)
-#define _accessDispatch22(f0, f1, ...) f1(__VA_ARGS__)
-#define accessDispatch(f0, f1, ...) cat3(_accessDispatch, accessLv, accessCtrl)(f0, f1, ##__VA_ARGS__)
-
-#define _dummyVoid(...)
-
-#define _dummyVirtual(ret, func, ...) void *func
-
-#define _export(ret, func, ...) \
-ret cat_2(className, func)(className *self, ##__VA_ARGS__)
-
-#define _virtual(ret, func, ...) ret (*func)(className *self, ##__VA_ARGS__)
-
-#define _bind(name) .name = name
-
-#define method(ret, func, ...) \
-ALIAS(func) _export(ret, func, ##__VA_ARGS__); \
-static ret func(className *self, ##__VA_ARGS__)
-
-#define $protected  1
-#define $private    2
-#define $public     0
-
-#define classVtabStart static const cat_2(className, vtab) vtab = {
-
-#define classVtabEnd };
-
-#define classVtabDefStart \
-typedef struct cat_2(className, vtab) cat_2(className, vtab); \
-struct cat_2(className, vtab) { \
-    _virtual(bool, ctor); \
-    _virtual(void, dtor); \
-    _virtual(bool, copy, className *other);
-
-#define classVtabDefEnd };
-
-typedef struct cat_2(objBase, vtab) cat_2(objBase, vtab);
-struct cat_2(objBase, vtab) {
+typedef struct objBase_vtab objBase_vtab;
+struct objBase_vtab {
     bool (*ctor)(objBase *);
     void (*dtor)(objBase *);
     bool (*copy)(objBase *, objBase *);
@@ -163,7 +120,7 @@ struct meta_class {
 
 struct objBase {
     const meta_type *meta;
-    const cat_2(objBase, vtab) *vptr;
+    const objBase_vtab *vptr;
 };
 
 struct meta_type {
--- common/framework/reflection/obj.h
+++ common/framework/reflection/obj.h
@@ -7,6 +7,51 @@
 
 #include "meta.h"
 
+#define _accessDispatch00(f0, f1, ...) f1(__VA_ARGS__)
+#define _accessDispatch01(f0, f1, ...) f1(__VA_ARGS__)
+#define _accessDispatch02(f0, f1, ...) f1(__VA_ARGS__)
+#define _accessDispatch10(f0, f1, ...) f0(__VA_ARGS__)
+#define _accessDispatch11(f0, f1, ...) f1(__VA_ARGS__)
+#define _accessDispatch12(f0, f1, ...) f1(__VA_ARGS__)
+#define _accessDispatch20(f0, f1, ...) f0(__VA_ARGS__)
+#define _accessDispatch21(f0, f1, ...) f0(__VA_ARGS__)
+#define _accessDispatch22(f0, f1, ...) f1(__VA_ARGS__)
+#define accessDispatch(f0, f1, ...) cat3(_accessDispatch, accessLv, accessCtrl)(f0, f1, ##__VA_ARGS__)
+
+#define _dummyVoid(...)
+
+#define _dummyVirtual(ret, func, ...) void *func
+
+#define _export(ret, func, ...) \
+ret cat_2(className, func)(className *self, ##__VA_ARGS__)
+
+#define _virtual(ret, func, ...) ret (*func)(className *self, ##__VA_ARGS__)
+
+#define _bind(name) .name = name
+
+#define method(ret, func, ...) \
+ALIAS(func) _export(ret, func, ##__VA_ARGS__); \
+static ret func(className *self, ##__VA_ARGS__)
+
+#define $protected  1
+#define $private    2
+#define $public     0
+
+#define classVtabStart static const cat_2(className, vtab) vtab = {
+
+#define classVtabEnd };
+
+#define classVtabDefStart \
+typedef struct cat_2(className, vtab) cat_2(className, vtab); \
+struct cat_2(className, vtab) { \
+    _virtual(bool, ctor); \
+    _virtual(void, dtor); \
+    _virtual(bool, copy, className *other);
+
+#define classVtabDefEnd };
+
+#define $(self, func, ...) (self)->vtab->func(self, ##__VA_ARGS__)
+
 void obj_dtor(void *obj);
 bool obj_ctor(void *obj);
 bool obj_copy(void *restrict dst, void *restrict src);
